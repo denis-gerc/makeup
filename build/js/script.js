afd9673c76fb0e208b4ccf1bff5641fbcfd83ad0
denis-gerc <gdv1001@gmail.com>
2020-11-08 21:28:31 +0300

    add submenu to html

--- build/js/script.js
+++ build/js/script.js
@@ -5,11 +5,17 @@ $(document).ready(function () {
 
     function myFunction() {
         let element = document.getElementById("mobile-menu__list");
+
         element.classList.toggle("open");
 
         x.classList.toggle("change");
+
+        $('body').toggleClass('lock');
+
     }
+
 });
+
 
 
 
